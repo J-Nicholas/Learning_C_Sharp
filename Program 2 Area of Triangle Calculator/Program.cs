3cdb767e2e6785d7d030dd4b62766e619d1fbc75
Johnathan Nicholas <jtnicholas2014@gmail.com>
2018-05-17 18:27:59 +0100

    Add project files.

---
+++ Program 2 Area of Triangle Calculator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_2_Area_of_Triangle_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
