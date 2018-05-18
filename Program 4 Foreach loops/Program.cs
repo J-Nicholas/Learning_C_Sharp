3281a1f69f8d251637e58a1bbfa8739e35b9f0d3
Johnathan Nicholas <jtnicholas2014@gmail.com>
2018-05-18 19:58:41 +0100

    Add project files.

---
+++ Program 4 Foreach loops/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4_Foreach_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
