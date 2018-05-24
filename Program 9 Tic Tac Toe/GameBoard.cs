7ade392b1d28e6fa1525203ba902fffaede8f11a
Johnathan Nicholas <jtnicholas2014@gmail.com>
2018-05-24 18:17:28 +0100

    Programmed a tic tac toe console based game with input validation using classes and various methods.

---
+++ Program 9 Tic Tac Toe/GameBoard.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_9_Tic_Tac_Toe
+{
+    class GameBoard
+    {
+        private string[] layout;
+        private string[] helpLayout;
+        private bool one, two , three, four, five, six, seven, eight, nine;
+
+        public GameBoard ()
+        {
+            layout = new string[5];
+            helpLayout = new string[5];
+            one = two = three = four = five = six = seven = eight = nine = false;
+
+        int countHelper = 7;
+            for (int row = 0; row < layout.Length; row++)
+            {
+                if (row % 2 == 0)
+                {
+                    layout[row] = "\t\t\t   |   |   ";
+                    helpLayout[row] = $"\t\t\t {countHelper} | {countHelper +1} | {countHelper + 2} ";
+                    countHelper -= 3;
+                }
+                else
+                {
+                    layout[row] = "\t\t\t---+---+---";
+                    helpLayout[row] = "\t\t\t---+---+---";
+                }
+            }
+        }
+        public void PrintGameBoard()                            // default empty gameboard
+        {
+            for (int i = 0; i < layout.Length; i++)
+            {
+                Console.WriteLine(layout[i]);
+            }
+        }
+        public void PrintHelpBoard()                            // displays gameboard with numbers in slots.
+        {
+            for (int i = 0; i < layout.Length; i++)
+            {
+                Console.WriteLine(helpLayout[i]);
+            }
+        }
+        public bool UpdateBoard(char token, string index )
+        {
+            // Bottom row
+            if (index == "1" && !one)
+            {
+                StringBuilder replacement = new StringBuilder(layout[4]);
+                replacement[4] = token;
+                layout[4] = replacement.ToString();
+                one = true;
+                return true;
+            }
+            else if (index == "2" && two == false)
+            {
+                StringBuilder replacement = new StringBuilder(layout[4]);
+                replacement[4 + 4] = token;
+                layout[4] = replacement.ToString();
+                two = true;
+                return true;
+            }
+            else if (index == "3" && !three)
+            {
+                StringBuilder replacement = new StringBuilder(layout[4]);
+                replacement[4 + 8] = token;
+                layout[4] = replacement.ToString();
+                three = true;
+                return true;
+            }
+            //middle row
+            else if (index == "4" && !four)
+            {
+                StringBuilder replacement = new StringBuilder(layout[2]);
+                replacement[4] = token;
+                layout[2] = replacement.ToString();
+                four = true;
+                return true;
+            }
+            else if (index == "5" && !five)
+            {
+                StringBuilder replacement = new StringBuilder(layout[2]);
+                replacement[4 + 4] = token;
+                layout[2] = replacement.ToString();
+                five = true;
+                return true;
+            }
+            else if (index == "6" && !six)
+            {
+                StringBuilder replacement = new StringBuilder(layout[2]);
+                replacement[4 + 8] = token;
+                layout[2] = replacement.ToString();
+                six = true;
+                return true;
+            }
+            // top row
+            else if  (index == "7" && ! seven)
+            {
+                StringBuilder replacement = new StringBuilder(layout[0]);
+                replacement[4] = token;
+                layout[0] = replacement.ToString();
+                seven = true;
+                return true;
+            }
+            else if (index == "8" && !eight)
+            {
+                StringBuilder replacement = new StringBuilder(layout[0]);
+                replacement[4+4] = token;
+                layout[0] = replacement.ToString();
+                eight = true;
+                return true;
+            }
+            else if (index == "9" && !nine)
+            {
+                StringBuilder replacement = new StringBuilder(layout[0]);
+                replacement[4 + 8] = token;
+                layout[0] = replacement.ToString();
+                nine = true;
+                return true;
+            }
+            else return false;
+        }
+        public bool CheckIfGameOver (int currentPlayer)
+        {
+            // ----------------------DIAGONAL RULES ---------------------------------
+            // Right leaning diagonal
+            if (layout[0][12] == 'X' && layout[2][8] == 'X' && layout[4][4] == 'X' )
+            {
+                Console.WriteLine($"Player {currentPlayer+1} won on the diagonal with X\'s.");
+                return true;
+            }
+            if (layout[0][12] == 'O' && layout[2][8] == 'O' && layout[4][4] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the diagonal with O\'s.");
+                return true;
+            }
+            // Left leaning diagonal
+            if (layout[0][4] == 'X' && layout[2][8] == 'X' && layout[4][12] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the diagonal with X\'s.");
+                return true;
+            }
+            if (layout[0][4] == 'O' && layout[2][8] == 'O' && layout[4][12] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the diagonal with O\'s.");
+                return true;
+            }
+
+            // ----------------------HORIZONTAL RULES --------------------------------
+            // top row
+            if (layout[0][4] == 'X' && layout[0][8] == 'X' && layout[0][12] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the top row with X\'s.");
+                return true;
+            }
+            if (layout[0][4] == 'O' && layout[0][8] == 'O' && layout[0][12] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the top row with O\'s.");
+                return true;
+            }
+            // middle row
+            if (layout[2][4] == 'X' && layout[2][8] == 'X' && layout[2][12] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the middle row with X\'s.");
+                return true;
+            }
+            if (layout[2][4] == 'O' && layout[2][8] == 'O' && layout[2][12] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the middle row with O\'s.");
+                return true;
+            }
+            // bottom row
+            if (layout[4][4] == 'X' && layout[4][8] == 'X' && layout[4][12] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the bottom row with X\'s.");
+                return true;
+            }
+            if (layout[4][4] == 'O' && layout[4][8] == 'O' && layout[4][12] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the bottom row with O\'s.");
+                return true;
+            }
+
+            // ----------------------VERTICAL RULES ----------------------------------
+            // left collumn
+            if (layout[0][4] == 'X' && layout[2][4] == 'X' && layout[4][4] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the left collumn with X\'s.");
+                return true;
+            }
+            if (layout[0][4] == 'O' && layout[2][4] == 'O' && layout[4][4] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the left collumn with O\'s.");
+                return true;
+            }
+            // middle collumn
+            if (layout[0][8] == 'X' && layout[2][8] == 'X' && layout[4][8] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the middle collumn with X\'s.");
+                return true;
+            }
+            if (layout[0][8] == 'O' && layout[2][8] == 'O' && layout[4][8] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the middle collumn with O\'s.");
+                return true;
+            }
+            // bottom row
+            if (layout[0][12] == 'X' && layout[2][12] == 'X' && layout[4][12] == 'X')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the right collumn with X\'s.");
+                return true;
+            }
+            if (layout[0][12] == 'O' && layout[2][12] == 'O' && layout[4][12] == 'O')
+            {
+                Console.WriteLine($"Player {currentPlayer + 1} won on the right collumn with O\'s.");
+                return true;
+            }
+
+            // ----------------------STALEMATE RULES -----------------------------------
+
+            return false;
+        }
+    }
+}
--- Program 9 Tic Tac Toe/Program.cs
+++ Program 9 Tic Tac Toe/Program.cs
@@ -10,6 +10,233 @@ namespace Program_9_Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            GameBoard display = new GameBoard();
+            Console.WriteLine("Welcome to the Tic Tac Toe Game.\n");
+            string userInput = null;
+            string[] currentPlayer = { "One", "Two" };
+            char playerOneToken = ' ';                                  // these get assigned later depending on user choice
+            char playerTwoToken = ' ';                                  // these get assigned later depending on user choice
+            int playerOneWins = 0;
+            int playerTwoWins = 0;
+            int drawnMatches = 0;
+            int turnCount = 0;
+
+            int playerTurn = 0;                                          // determines whose turn it is, playerOne = 0, PlayerTwo = 1;
+            bool isGameOver = false;
+            bool firstPlayerDetermined = false;
+            bool chosenTokenDetermined = false;
+            bool validPlayAgain = false;
+            bool firstGame = true;
+            bool gameWasDraw = false;
+
+            while (userInput != "exit")
+            {
+                PrintMenu();
+                if (firstGame == false)
+                {
+                    Console.Clear();
+                    PrintMenu();
+                }
+                userInput = Console.ReadLine();
+
+
+                if (userInput == "exit")                                // Exit functionality
+                {
+                    Console.WriteLine("Exiting.");
+                    continue;
+                }
+                else if (userInput == "help")                           // display help menu
+                {
+                    PrintHelp();
+                }
+                else if (userInput == "start")
+                {
+                    while (isGameOver == false)
+                    {
+                        if (firstPlayerDetermined == false)
+                        {
+                            while (firstPlayerDetermined == false)
+                            {
+                                Console.Write("Who should go first? Player One or Player Two?\nEnter \"1\" or \"2\":");
+                                userInput = Console.ReadLine();
+                                if (userInput == "2" || userInput == "two" || userInput == "Two")
+                                {
+                                    playerTurn++;
+                                    firstPlayerDetermined = true;
+                                }
+                                else if (userInput == "1" || userInput == "one" || userInput == "Two")
+                                {
+                                    firstPlayerDetermined = true;
+                                }
+                                else
+                                    Console.WriteLine("Invalid input, try again.");
+                            }
+                        }
+                        if (chosenTokenDetermined == false)
+                        {
+                            Console.Write("Which token would you like? X\'s or O\'s?\nType \"X\" or \"0\"");
+                            userInput = Console.ReadLine();
+                            if (userInput == "x" || userInput == "X")
+                            {
+                                if (playerTurn == 0)
+                                {
+                                    playerOneToken = 'X';
+                                    playerTwoToken = 'O';
+                                }
+                                else
+                                {
+                                    playerTwoToken = 'X';
+                                    playerOneToken = 'O';
+                                }
+                            }
+                            else if (userInput == "o" || userInput == "O")
+                            {
+                                if (playerTurn == 0)
+                                {
+                                    playerOneToken = 'O';
+                                    playerTwoToken = 'X';
+                                }
+                                else
+                                {
+                                    playerOneToken = 'X';
+                                    playerTwoToken = 'O';
+                                }
+                            }
+                            else continue;
+                            Console.Clear();
+                            Console.WriteLine($"Player One has {playerOneToken}\'s.\nPlayer Two has {playerTwoToken}\'s.");
+                            chosenTokenDetermined = true;
+                        }               // logic for setting player's chosen tokens
+                        
+
+                        display.PrintGameBoard();
+
+
+                        Console.WriteLine($"Player {currentPlayer[playerTurn]}\'s turn:\n");
+                        if (playerTurn == 0 % 2)
+                        {
+                            Console.WriteLine($"Enter a number to place an {playerOneToken}");
+                            bool validInput = false;
+                            while (validInput == false)
+                            {
+                                userInput = Console.ReadLine();
+                                if (display.UpdateBoard(playerOneToken, userInput))
+                                    validInput = true;                          //repeat until valid input is received 
+                                if (display.CheckIfGameOver(playerTurn) == true)
+                                {
+                                    isGameOver = true;
+                                    validInput = true;
+                                    continue;
+                                }
+                            }
+                        }                           // logic for validating token placement and checking for winner
+                        if (playerTurn == 1 % 2)
+                        {
+                            Console.WriteLine($"Enter a number to place an {playerTwoToken}");
+                            bool validInput = false;
+                            while (validInput == false)
+                            {
+
+                                userInput = Console.ReadLine();
+                                if (display.UpdateBoard(playerTwoToken, userInput))
+                                    validInput = true;                          //repeat until valid input is received 
+                                if (display.CheckIfGameOver(playerTurn) == true)
+                                {
+                                    isGameOver = true;
+                                    validInput = true;
+                                    break;
+                                }
+                            }
+                        }                           // logic for validating token placement and checking for winner
+
+                        Console.Clear();
+                        //stalemate rules
+                        if (turnCount == 8 && display.CheckIfGameOver(playerTurn) == false)
+                        {
+                            display.PrintGameBoard();
+                            Console.WriteLine("Match ended in stalemate.\nEnter any key to continue...");
+                            Console.ReadLine();
+                            Console.Clear();
+                            drawnMatches++;
+                            gameWasDraw = true;
+                            isGameOver = true;
+                        }
+                        if (isGameOver == false)
+                        {
+                            turnCount++;
+                            playerTurn++;                                             //
+                            playerTurn %= 2;                                          // manages current player   
+                        }
+                    }
+
+                    // After game logic
+                    Console.Clear();
+                    display.PrintGameBoard();
+                    Console.WriteLine();
+                    display.CheckIfGameOver(playerTurn);
+                    turnCount = 0;
+                    firstGame = false;
+
+                    if (playerTurn == 0 && gameWasDraw == false)
+                    {
+                        playerOneWins++;
+                    }
+                    else if (playerTurn == 1 && gameWasDraw == false)
+                    {
+                        playerTwoWins++;
+                    }
+                    gameWasDraw = false; // resetting for next match
+
+                    Console.WriteLine($"Player One has {playerOneWins} wins.\nPlayer Two has {playerTwoWins} wins.\n");
+                    Console.WriteLine($"There have been {drawnMatches} draws.");
+
+                    Console.WriteLine("Play again? y/n");
+                    while(validPlayAgain == false)
+                    {
+                        userInput = Console.ReadLine();
+                        if (userInput == "y")
+                        {
+                            userInput = "start";
+                            validPlayAgain = true;
+                            isGameOver = false;
+                            display = new GameBoard();
+                            continue;
+                        }
+                        else if (userInput == "n")
+                        {
+                            userInput = "exit";
+                            validPlayAgain = true;
+                            continue;
+                        }
+                        else Console.WriteLine("Invalid input, try again.\n");
+                    }
+                    validPlayAgain = false;                                             // resetting for next match
+                    playerTurn++;                                                       // alternating who goes first
+                    playerTurn %= 2;
+                    continue;
+                }
+            }
+        }
+        static void PrintHelp()
+        {
+            GameBoard helpBoard = new GameBoard();
+            string helpMenuInput = null;
+
+            while (helpMenuInput != "Okay")
+            {
+                Console.Clear();
+                Console.WriteLine("Use the numpad to select which square to place a cross or circle in.\n");
+                helpBoard.PrintHelpBoard();
+                Console.WriteLine("\nType \"Okay\" to continue.");
+                helpMenuInput = Console.ReadLine();
+            }
+            Console.Clear();
+            
+        }
+        static void PrintMenu()
+        {
+            Console.WriteLine("Type \"help\" to view the controls \nor type \"start\" to begin");
         }
     }
+
 }
